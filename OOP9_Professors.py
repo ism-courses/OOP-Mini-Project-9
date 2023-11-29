47fc9848e6cb51a115eaaed5b3224c622bf49465
ism-courses <152141243+ism-courses@users.noreply.github.com>
2023-11-29 12:54:58 -0700

    Add files via upload

---
+++ OOP9_Professors.py
@@ -0,0 +1,11 @@
+class Professor:
+    
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+        
+    def greet_students(self):
+        print("Welcome to class!")
+
+# Write your code below:
